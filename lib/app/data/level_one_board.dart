05f502ec18f663ce0a2e277d7040748e7578f215
Wolfiz Developer <Wolfiz_Developer@Naveeds-MacBook-Pro.local>
2022-02-23 17:45:25 +0500

    Update

--- lib/app/data/level_one_board.dart
+++ lib/app/data/level_one_board.dart
@@ -34,58 +34,58 @@ Board levelOneBoard({required VoidCallback onWin}) => Board(
             Location(rowIndex: 1, columnIndex: 2),
           ],
         ),
-        // Block(
-        //   lottiePath: 'tree1',
-        //   location: [
-        //     Location(rowIndex: 0, columnIndex: 3),
-        //     Location(rowIndex: 1, columnIndex: 3),
-        //   ],
-        // ),
-        // Block(
-        //   lottiePath: 'stone1',
-        //   location: [
-        //     Location(rowIndex: 2, columnIndex: 1),
-        //     Location(rowIndex: 2, columnIndex: 2),
-        //   ],
-        // ),
-        // Block(
-        //   lottiePath: 'spider1',
-        //   location: [
-        //     Location(rowIndex: 3, columnIndex: 0),
-        //     Location(rowIndex: 4, columnIndex: 0),
-        //   ],
-        // ),
-        // Block(
-        //   lottiePath: 'wolf6',
-        //   location: [
-        //     Location(rowIndex: 3, columnIndex: 3),
-        //     Location(rowIndex: 4, columnIndex: 3),
-        //   ],
-        // ),
-        // Block(
-        //   lottiePath: 'bat1',
-        //   location: [
-        //     Location(rowIndex: 3, columnIndex: 1),
-        //   ],
-        // ),
-        // Block(
-        //   lottiePath: 'hand',
-        //   location: [
-        //     Location(rowIndex: 3, columnIndex: 2),
-        //   ],
-        // ),
-        // Block(
-        //   lottiePath: 'wolf_new3',
-        //   location: [
-        //     Location(rowIndex: 4, columnIndex: 1),
-        //   ],
-        // ),
-        // Block(
-        //   lottiePath: 'wolf_new4',
-        //   location: [
-        //     Location(rowIndex: 4, columnIndex: 2),
-        //   ],
-        // ),
+        Block(
+          lottiePath: 'tree1',
+          location: [
+            Location(rowIndex: 0, columnIndex: 3),
+            Location(rowIndex: 1, columnIndex: 3),
+          ],
+        ),
+        Block(
+          lottiePath: 'stone1',
+          location: [
+            Location(rowIndex: 2, columnIndex: 1),
+            Location(rowIndex: 2, columnIndex: 2),
+          ],
+        ),
+        Block(
+          lottiePath: 'spider1',
+          location: [
+            Location(rowIndex: 3, columnIndex: 0),
+            Location(rowIndex: 4, columnIndex: 0),
+          ],
+        ),
+        Block(
+          lottiePath: 'wolf6',
+          location: [
+            Location(rowIndex: 3, columnIndex: 3),
+            Location(rowIndex: 4, columnIndex: 3),
+          ],
+        ),
+        Block(
+          lottiePath: 'bat1',
+          location: [
+            Location(rowIndex: 3, columnIndex: 1),
+          ],
+        ),
+        Block(
+          lottiePath: 'hand',
+          location: [
+            Location(rowIndex: 3, columnIndex: 2),
+          ],
+        ),
+        Block(
+          lottiePath: 'wolf_new3',
+          location: [
+            Location(rowIndex: 4, columnIndex: 1),
+          ],
+        ),
+        Block(
+          lottiePath: 'wolf_new4',
+          location: [
+            Location(rowIndex: 4, columnIndex: 2),
+          ],
+        ),
         SolidBlock(
           location: [
             Location(rowIndex: 5, columnIndex: 0),
